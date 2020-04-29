f3ea6bddd4b61c8158f0927628394736799fdfcb
Anatolie1 <gavriluta.anatolie@gmail.com>
2020-04-29 23:16:50 +0200

    small changes

--- Program.cs
+++ Program.cs
@@ -1,11 +1,7 @@
-﻿using Newtonsoft.Json;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Net;
 using System.Net.Http;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
